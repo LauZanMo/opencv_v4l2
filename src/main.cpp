8ed687f2fb6b6d333ba8259fed0fced3a4ac5204
LauZanMo <LauZanMo@whu.edu.cn>
2022-04-27 17:24:45 +0800

    添加v4l2相机捕捉

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+#include <boost/thread.hpp>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+
+void CamCapture() {
+    cv::VideoCapture cap;
+    cv::Mat          frame;
+    cap.open(0, cv::CAP_V4L2);
+
+    if (cap.isOpened()) {
+        cout << "size=" << cap.get(cv::CAP_PROP_FRAME_WIDTH) << "*"
+             << cap.get(cv::CAP_PROP_FRAME_HEIGHT) << endl;
+        cv::namedWindow("Video");
+    } else {
+        return;
+    }
+
+    while (1) {
+        cap >> frame;
+        if (frame.empty() || cv::waitKey(1))
+            break;
+        cv::imshow("Video", frame);
+
+        boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+    }
+
+    cv::destroyAllWindows();
+    cap.release();
+}
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, world!" << std::endl;
+    boost::thread t(CamCapture);
+    t.join();
     return 0;
 }
